34ae03d825cbc97ff008ef9dd31ae7e30fdee50e
jingxinwang <wjxalexander@hotmail.com>
2024-05-28 20:34:04 +0800

    parse let statement

---
+++ monkey/src/ast/ast.go
@@ -0,0 +1,67 @@
+package ast
+
+import "writing_an_interpreter_in_go/monkey/src/token"
+
+type Node interface {
+	TokenLiteral() string
+}
+
+type Statement interface {
+	Node
+	statementNode()
+}
+
+type Expression interface {
+	Node
+	expressionNode()
+}
+
+type Program struct {
+	Statements []Statement
+}
+
+type LetStatement struct {
+	Token token.Token
+	Name  *Identifier
+	Value Expression
+}
+type Identifier struct {
+	Token token.Token
+	Value string
+}
+
+func (p *Program) TokenLiteral() string {
+	if len(p.Statements) > 0 {
+		/**
+		In Go, when embedding an interface within another interface,
+		you don't access the embedded methods through the embedded interface name.
+		Instead, you directly access the methods as if they were defined in the embedding interface itself.
+
+		In the code you provided,
+		the Node interface is embedded within the Statement interface using the line Node.
+		This means that any type implementing the Statement interface will also implicitly implement the Node interface and its methods.
+		*/
+
+		return p.Statements[0].TokenLiteral()
+	} else {
+		return ""
+	}
+}
+
+// satisfy Statement
+func (ls *LetStatement) statementNode() {
+
+}
+
+// satisfy Node
+func (ls *LetStatement) TokenLiteral() string {
+	return ls.Token.Literal
+}
+
+func (i *Identifier) expressionNode() {
+
+}
+
+func (i *Identifier) TokenLiteral() string {
+	return i.Token.Literal
+}
---
+++ monkey/src/parser/parser.go
@@ -0,0 +1,86 @@
+package parser
+
+import (
+	"writing_an_interpreter_in_go/monkey/src/ast"
+	"writing_an_interpreter_in_go/monkey/src/lexer"
+	"writing_an_interpreter_in_go/monkey/src/token"
+)
+
+type Parser struct {
+	l         *lexer.Lexer //pointer to an instance of the lexer
+	curToken  token.Token
+	peekToken token.Token
+}
+
+func New(l *lexer.Lexer) *Parser {
+	p := &Parser{l: l}
+	// Read two tokens, so curToken and peekToken are both set
+	p.nextToken()
+	p.nextToken()
+	return p
+}
+
+func (p *Parser) nextToken() {
+	p.curToken = p.peekToken
+	p.peekToken = p.l.NextToken()
+}
+
+func (p *Parser) ParseProgram() *ast.Program {
+	//creates a new instance of the ast.Program struct and returns its address using the & operator.
+	program := &ast.Program{}
+	program.Statements = []ast.Statement{}
+	for p.curToken.Type != token.EOF {
+		stmt := p.parseStatement()
+		if stmt != nil {
+			program.Statements = append(program.Statements, stmt)
+		}
+		p.nextToken()
+	}
+	return program
+}
+
+func (p *Parser) parseStatement() ast.Statement {
+	switch p.curToken.Type {
+	case token.LET:
+		return p.parseLetStatement()
+	default:
+		return nil
+	}
+}
+
+/**
+* This function signature indicates that the parseLetStatement() method returns a pointer to ast.Statement.
+* The * before ast.Statement denotes that the value being returned is a memory address pointing to an ast.Statement object.
+* This allows the function to return nil if necessary, indicating the absence of a valid ast.Statement object.
+**/
+
+func (p *Parser) parseLetStatement() *ast.LetStatement {
+	stmt := &ast.LetStatement{Token: p.curToken}
+	if !p.expectPeek(token.IDENT) {
+		return nil
+	}
+	stmt.Name = &ast.Identifier{Token: p.curToken, Value: p.curToken.Literal}
+	if !p.expectPeek(token.ASSIGN) {
+		return nil
+	}
+	if !p.curTokenIs(token.SEMICOLON) {
+		p.nextToken()
+	}
+	return stmt
+}
+
+func (p *Parser) curTokenIs(t token.TokenType) bool {
+	return p.peekToken.Type == t
+}
+
+func (p *Parser) peekTokenIs(t token.TokenType) bool {
+	return p.peekToken.Type == t
+}
+
+func (p *Parser) expectPeek(t token.TokenType) bool {
+	if p.peekTokenIs(t) {
+		p.nextToken()
+		return true
+	}
+	return false
+}
