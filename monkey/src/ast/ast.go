25301d532a12879303d46aceb2db5e1625c26465
jingxinwang <wjxalexander@hotmail.com>
2024-06-12 21:48:13 +0800

    toString

--- monkey/src/ast/ast.go
+++ monkey/src/ast/ast.go
@@ -1,9 +1,13 @@
 package ast
 
-import "writing_an_interpreter_in_go/monkey/src/token"
+import (
+	"bytes"
+	"writing_an_interpreter_in_go/monkey/src/token"
+)
 
 type Node interface {
 	TokenLiteral() string
+	String() string
 }
 
 type Statement interface {
@@ -36,6 +40,11 @@ type ReturnStatment struct {
 	ReturnValue Expression
 }
 
+type ExpressionStatement struct {
+	Token      token.Token
+	Expression Expression
+}
+
 func (p *Program) TokenLiteral() string {
 	if len(p.Statements) > 0 {
 		/**
@@ -64,6 +73,18 @@ func (ls *LetStatement) TokenLiteral() string {
 	return ls.Token.Literal
 }
 
+func (ls *LetStatement) String() string {
+	var out bytes.Buffer
+	out.WriteString(ls.TokenLiteral() + " ")
+	out.WriteString(ls.Name.String())
+	out.WriteString(" = ")
+	if ls.Value != nil {
+		out.WriteString(ls.Value.String())
+	}
+	out.WriteString(";")
+	return out.String()
+}
+
 func (i *Identifier) expressionNode() {
 
 }
@@ -72,10 +93,46 @@ func (i *Identifier) TokenLiteral() string {
 	return i.Token.Literal
 }
 
+func (i *Identifier) String() string {
+	return i.Value
+}
+
 func (rs *ReturnStatment) statementNode() {
 
 }
 
 func (rs *ReturnStatment) TokenLiteral() string {
 	return rs.Token.Literal
+}
+
+func (rs *ReturnStatment) String() string {
+	var out bytes.Buffer
+	out.WriteString(rs.TokenLiteral() + " ")
+	if rs.ReturnValue != nil {
+		out.WriteString(rs.ReturnValue.String())
+	}
+	out.WriteString(";")
+	return out.String()
+}
+
+func (es *ExpressionStatement) statementNode() {
+
+}
+func (es *ExpressionStatement) TokenLiteral() string {
+	return es.Token.Literal
+}
+
+func (es *ExpressionStatement) String() string {
+	if es.Expression != nil {
+		return es.Expression.String()
+	}
+	return ""
+}
+
+func (p *Program) String() string {
+	var out bytes.Buffer
+	for _, s := range p.Statements {
+		out.WriteString(s.String())
+	}
+	return out.String()
 }
---
+++ monkey/src/ast/ast_test.go
@@ -0,0 +1,36 @@
+package ast
+
+import (
+	"testing"
+	"writing_an_interpreter_in_go/monkey/src/token"
+)
+
+func TestString(t *testing.T) {
+	program := &Program{
+		Statements: []Statement{
+			&LetStatement{
+				Token: token.Token{
+					Type:    token.LET,
+					Literal: "let",
+				},
+				Name: &Identifier{
+					Token: token.Token{
+						Type:    token.IDENT,
+						Literal: "myVar",
+					},
+					Value: "myVal",
+				},
+				Value: &Identifier{
+					Token: token.Token{
+						Type:    token.IDENT,
+						Literal: "anotherVar",
+					},
+					Value: "anotherVar",
+				},
+			},
+		},
+	}
+	if program.String() != "let myVal = anotherVar;" {
+		t.Errorf("program.String() wrong. got=%q", program.String())
+	}
+}
